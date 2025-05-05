783902be9e668f6b8016f29621aad6b54bad2c77
OrryFrasetyo <orrytio12345@gmail.com>
2025-05-05 13:44:01 +0700

    build (module) : upgrade go version to 1.24.2

--- middlewares/auth_middleware.go
+++ middlewares/auth_middleware.go
@@ -14,39 +14,39 @@ func JWTAuth() gin.HandlerFunc {
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
 			c.JSON(http.StatusUnauthorized, gin.H{
-				"error": true,
+				"error":   true,
 				"message": "Authorization header is required",
 			})
 			c.Abort()
 			return
 		}
-		
+
 		// Format Authorization header should "Bearer [token]"
 		parts := strings.Split(authHeader, " ")
 		if len(parts) != 2 || parts[0] != "Bearer" {
 			c.JSON(http.StatusUnauthorized, gin.H{
-				"error": true,
+				"error":   true,
 				"message": "Authorization header format must be Bearer [token]",
 			})
 			c.Abort()
 			return
 		}
-		
+
 		// validation token
 		claims, err := utils.ValidateToken(parts[1])
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{
-				"error": true,
+				"error":   true,
 				"message": "Invalid or expired token",
 			})
 			c.Abort()
 			return
 		}
-		
+
 		// Set employee ID and email to conteks for use by handler
 		c.Set("employeeId", claims.Id)
 		c.Set("employeeEmail", claims.Email)
-		
+
 		c.Next()
 	}
 }
