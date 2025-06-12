bb5df4f821b5c1a3e01927198e871e325fbaa464
OrryFrasetyo <orrytio12345@gmail.com>
2025-06-12 10:00:01 +0700

    refactor(database): consolidate database connection configuration using MYSQL_URL

--- models/setup.go
+++ models/setup.go
@@ -11,15 +11,12 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dbUser := os.Getenv("MYSQL_USERNAME")  
-	dbPass := os.Getenv("MYSQL_PASSWORD")
-	dbHost := os.Getenv("MYSQL_HOST")     
-	dbPort := os.Getenv("MYSQL_PORT")     
-	dbName := os.Getenv("MYSQL_DATABASE")  
-	
-	// dsn := "root:@tcp(127.0.0.1:3306)/hotelqu_db?parseTime=true"
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		dbUser, dbPass, dbHost, dbPort, dbName)
+	dsn := os.Getenv("MYSQL_URL")
+
+	// Pastikan dsn tidak kosong
+	if dsn == "" {
+		panic("MYSQL_URL environment variable is not set")
+	}
 
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
