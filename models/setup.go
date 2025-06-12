3a56233829ba8dbe65c1a5f65c97b31a35ef6e8b
OrryFrasetyo <orrytio12345@gmail.com>
2025-06-12 14:50:12 +0700

    refactor(database): remove commented-out ConnectDatabase function

--- models/setup.go
+++ models/setup.go
@@ -57,30 +57,3 @@ func ConnectDatabase() {
 	fmt.Println("Database migration completed successfully")
 	DB = database
 }
-// func ConnectDatabase() {
-// 	dsn := os.Getenv("mysql_url")
-
-// 	log.Printf("DEBUG: MYSQL_URL value from environment: %s", dsn)
-// 	// Pastikan dsn tidak kosong
-// 	if dsn == "" {
-// 		log.Println("ERROR: MYSQL_URL environment variable is empty!")
-// 		panic("MYSQL_URL environment variable is not set. Please check Railway variables.")
-// 	}
-
-// 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-// 	if err != nil {
-// 		log.Printf("ERROR: Failed to connect to database using DSN: %s", dsn)
-// 		panic(fmt.Sprintf("failed to connect database : %v", err))
-// 	}
-
-// 	fmt.Println("Starting database migration...")
-
-// 	err = database.AutoMigrate(&Department{}, &Position{}, &Shift{}, &Employee{}, &Schedule{}, &Attendance{})
-// 	if err != nil {
-// 			panic("failed to migrate Department, Position, Shift, Employee, " + err.Error())
-// 	}
-	
-// 	fmt.Println("Database migration completed successfully")
-
-// 	DB = database
-// }
